72bf471f84b7a2d670be5726e0828492f092659d
Susmitabhujel22 <163893066+Susmitabhujel22@users.noreply.github.com>
2026-02-23 09:21:44 +0545

    Add files via upload

---
+++ lab3/Fraction Knapsack.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+struct Item
+{
+    int weight;
+    int profit;
+    float ratio;
+};
+
+void sortItems(Item arr[], int n)
+{
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = i + 1; j < n; j++)
+        {
+            if(arr[i].ratio < arr[j].ratio)
+            {
+                Item temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n, capacity;
+
+    cout << "Enter number of items: ";
+    cin >> n;
+
+    Item arr[n];
+
+    cout << "Enter weight and profit of each item:\n";
+    cout<<"Wi"<<" "<<"Vi\n";
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i].weight >> arr[i].profit;
+        arr[i].ratio = (float)arr[i].profit / arr[i].weight;
+    }
+
+    cout << "Enter knapsack capacity: ";
+    cin >> capacity;
+
+    sortItems(arr, n);
+
+    float totalProfit = 0.0;
+
+    for(int i = 0; i < n; i++)
+    {
+        if(capacity >= arr[i].weight)
+        {
+            totalProfit += arr[i].profit;
+            capacity -= arr[i].weight;
+        }
+        else
+        {
+            totalProfit += arr[i].ratio * capacity;
+            break;
+        }
+    }
+
+    cout << "Maximum Profit = " << totalProfit;
+
+    return 0;
+}
+
---
+++ lab3/Heap_sort.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+using namespace std;
+
+void maxHeapify(int arr[], int n, int i)
+{
+    int largest = i;
+    int left = 2*i + 1;
+    int right = 2*i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i)
+    {
+        swap(arr[i], arr[largest]);
+        maxHeapify(arr, n, largest);
+    }
+}
+
+void minHeapify(int arr[], int n, int i)
+{
+    int smallest = i;
+    int left = 2*i + 1;
+    int right = 2*i + 2;
+
+    if (left < n && arr[left] < arr[smallest])
+        smallest = left;
+
+    if (right < n && arr[right] < arr[smallest])
+        smallest = right;
+
+    if (smallest != i)
+    {
+        swap(arr[i], arr[smallest]);
+        minHeapify(arr, n, smallest);
+    }
+}
+
+void maxHeapSort(int arr[], int n)
+{
+    for (int i = n/2 - 1; i >= 0; i--)
+        maxHeapify(arr, n, i);
+
+    for (int i = n-1; i > 0; i--)
+    {
+        swap(arr[0], arr[i]);
+        maxHeapify(arr, i, 0);
+    }
+}
+
+void minHeapSort(int arr[], int n)
+{
+    for (int i = n/2 - 1; i >= 0; i--)
+        minHeapify(arr, n, i);
+
+    for (int i = n-1; i > 0; i--)
+    {
+        swap(arr[0], arr[i]);
+        minHeapify(arr, i, 0);
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n], arr2[n];
+
+    cout << "Enter elements:";
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        arr2[i] = arr[i];   // copy original array
+    }
+
+    maxHeapSort(arr, n);
+    minHeapSort(arr2, n);
+
+    cout << "\nSorted array(Max Heap):";
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    cout << "\n\nSorted array(Min Heap):";
+    for(int i = 0; i < n; i++)
+        cout << arr2[i] << " ";
+
+    return 0;
+}
+
