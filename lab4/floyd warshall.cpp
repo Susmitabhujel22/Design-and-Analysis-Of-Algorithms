68214c9c3cef6b660398e6c09170cbf4f19c1006
Susmitabhujel22 <163893066+Susmitabhujel22@users.noreply.github.com>
2026-03-24 16:43:46 +0545

    Add files via upload

---
+++ lab4/floyd warshall.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+#define INF 999
+
+int main() {
+    int V;
+    cout << "Enter number of vertices: ";
+    cin >> V;
+
+    int graph[V][V];
+    cout << "Enter the adjacency matrix (use " << INF << " for infinity):\n";
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            cin >> graph[i][j];
+        }
+    }
+
+    int dist[V][V];
+
+    // Initialize distance matrix
+    for (int i = 0; i < V; i++)
+        for (int j = 0; j < V; j++)
+            dist[i][j] = graph[i][j];
+
+    // Floyd–Warshall Algorithm
+    for (int k = 0; k < V; k++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][k] != INF && dist[k][j] != INF &&
+                    dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    // Print result
+    cout << "\nShortest distance matrix:\n";
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                cout << "INF ";
+            else
+                cout << dist[i][j] << "   ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
